a09dd2ce47c2c8594cd3804ced53793ed8025c73
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-08-31 19:18:37 -0400

    Add character creation integration tests

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -4,8 +4,8 @@ feature "Signing in" do
   let(:user) { FactoryGirl.create(:user) }
   it "signs in valid users" do
     visit root_path
-    expect(page).to_not have_content("DungeonMaster")
+    expect(page).to_not have_content("Create Character")
     login(user)
-    expect(page).to have_content("DungeonMaster")
+    expect(page).to have_content("Create Character")
   end
 end
